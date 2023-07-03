3ad80cad6c686d0388f94b7c6a8af4d8cc4fd76b
root <root@ASPIRE>
2023-07-03 19:44:36 +0300

    Add kitty Action

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,6 +9,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 DEBUG = True
 
 ALLOWED_HOSTS = []
+#'dima.serveblog.net', '127.0.0.1', 'localhost'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,8 +57,12 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+	'HOST': os.getenv('DB_HOST', ''),
+	'POST': os.getenv('DB_PORT', 5433)
     }
 }
 
@@ -93,8 +98,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'collected_static'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+# os.path.join('/var/media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
