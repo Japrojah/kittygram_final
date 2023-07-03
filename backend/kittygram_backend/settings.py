cf42d15c7223bb3efcd93802c84d20931af79f46
root <root@ASPIRE>
2023-07-03 21:38:58 +0300

    Add basic tests Action

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,6 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 DEBUG = True
 
 ALLOWED_HOSTS = []
-#'dima.serveblog.net', '127.0.0.1', 'localhost'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -101,7 +100,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/media'
 # os.path.join('/var/media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
